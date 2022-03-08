8b1047cc1c3a3d6cd52e189bd94cac44bace3a6e
Ahmed <48225917+ahmedanadi@users.noreply.github.com>
2022-03-08 20:02:51 +0000

    added driver instance

---
+++ IHSAutomationTask/Tests/Tests.cs
@@ -0,0 +1,31 @@
+using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace IHSAutomationTask
+{
+    public class Tests
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            driver.Close();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
